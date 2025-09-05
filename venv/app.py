e77a9228703bfb953110b0b66fb662a055efbf85
Binula <binula20052003@outlook.com>
2025-09-05 22:00:12 +0100

    Update app.py

--- venv/app.py
+++ venv/app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
-@app.route("/")
+
+PET_RECORDS = {
+    "kitcat": 198,
+    "petz1": 430
+}
+
+@app.route("/", methods=["GET"])
 def home():
-    return "Hello World!"
+    return "Flask Test"
+
+
+@app.route("/status", methods=["POST"])
+def status():
+    
+    if not request.is_json: # Check if request has JSON data
+        abort(403)
+
+    data = request.get_json()
+
+    # Second Validator
+    if "command" not in data or "practice_id" not in data:
+        abort(403)
+    
+    if data["command"] != "status": # Check that command is status
+        abort(403)
+
+    id = data["practice_id"]
+    pets_loaded = PET_RECORDS.get(id, 0)
+
+
+    return jsonify({"pets_loaded_today": pets_loaded})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
