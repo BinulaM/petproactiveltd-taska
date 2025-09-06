91bdf29503218e22d43273cdd5ad2552875a94ac
Binula <binula20052003@outlook.com>
2025-09-06 02:42:59 +0100

    Updated app.py

--- venv/app.py
+++ venv/app.py
@@ -3,13 +3,13 @@ from flask import Flask, jsonify, request, abort
 app = Flask(__name__)
 
 
-PET_RECORDS = {
+PET_RECORDS = { # Mock Database
     "kitcat": 198,
     "petz1": 430
 }
 
 @app.route("/", methods=["GET"])
-def test():
+def root():
     return "Run Test"
 
 
@@ -17,22 +17,40 @@ def test():
 def status():
     
     if not request.is_json: # Check if request has JSON data
-        abort(403)
+        abort(403, description = "JSON not in request")
 
     data = request.get_json()
 
     # Second Validator
     if "command" not in data or "practice_id" not in data:
-        abort(403)
+        abort(403, description = "Missing 'command' or 'practice_id'")
     
     if data["command"] != "status": # Check that command is status
-        abort(403)
+        abort(403, description='Invalid "command" value')
 
-    id = data["practice_id"]
+    id = str(data["practice_id"]).lower().strip() # Whitespace and upper-case handling
     pets_loaded = PET_RECORDS.get(id, 0)
 
 
     return jsonify({"pets_loaded_today": pets_loaded})
+
+@app.route('/working', methods=['GET']) # check if API is working 
+def working():
+    return jsonify({"status": "working"})
+
+@app.route("/practices/<practice_id>", methods=["GET"])
+def get_practice(practice_id):
+    pets_loaded = PET_RECORDS.get(practice_id, 0)
+    return jsonify({"pets_loaded_today": pets_loaded})
+
+
+@app.errorhandler(403)
+def errorhandle(e):
+    return jsonify({
+        "error": "forbidden",
+        "detail": getattr(e, "description", "Forbidden")
+    }), 403
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
